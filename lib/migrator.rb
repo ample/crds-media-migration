4f1184c9af8b1acf9ac57079c52d032375c52ce6
Sean C Davis <scdavis41@gmail.com>
2018-06-21 14:41:52 -0400

    US13809 - Migrate Videos/Music Run through the entire thing

--- lib/migrator.rb
+++ lib/migrator.rb
@@ -10,20 +10,35 @@ require_relative '../models/video'
 class Migrator
 
   def self.migrate
-    # series_data = Exporter.get_entries('/series')
+    series_data = Exporter.get_entries('/series')
     # series = Series.new(series_data.first)
     # series.transform!
     # series.import!
+    series_data.each do |data|
+      series = Series.new(data)
+      series.transform!
+      series.import!
+    end
 
-    # videos_data = Exporter.get_entries('/videos')
+    videos_data = Exporter.get_entries('/videos')
     # video = Video.new(videos_data.first)
     # video.transform!
     # video.import!
+    videos_data.each do |data|
+      video = Video.new(data)
+      video.transform!
+      video.import!
+    end
 
     music_data = Exporter.get_entries('/music')
-    song = Song.new(music_data.first)
-    song.transform!
-    song.import!
+    # song = Song.new(music_data.first)
+    # song.transform!
+    # song.import!
+    music_data.each do |data|
+      song = Song.new(data)
+      song.transform!
+      song.import!
+    end
   end
 
 end
