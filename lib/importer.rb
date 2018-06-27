090bbec158f5fd5088c0927c37f33008aed30780
Sean C Davis <scdavis41@gmail.com>
2018-06-27 14:06:17 -0400

    Add separate tasks for processing and publishing assets Also attempt to speed up entry creation by memoizing env's content types

--- lib/importer.rb
+++ lib/importer.rb
@@ -6,8 +6,11 @@ require 'mime/types/full'
 class Importer
 
   class << self
+
+    # ---------------------------------------- | Creating
+
     def create_entry(content_type, data)
-      content_type = env.content_types.find(content_type)
+      content_type = content_types.find(content_type)
       content_type.entries.create(data)
     end
 
@@ -18,32 +21,49 @@ class Importer
       image_file.properties[:upload] = url
       title = File.basename(url, '.*').titleize
       asset = env.assets.create(title: title, file: image_file)
-      sleep 0.15
-      asset.process_file
-      sleep 0.15
-      asset.publish
-      Logger.write('.', :blue)
-      sleep 0.15
+      log_and_wait :blue
       asset
     end
+
+    # ---------------------------------------- | Processing
+
+    def process_assets
+      env.assets.all(limit: 1000).to_a.reject(&:published?).each do |asset|
+        asset.process_file
+        log_and_wait :blue
+      end
+    end
+
+    # ---------------------------------------- | Publishing
+
+    def publish_assets
+      env.assets.all(limit: 1000).to_a.reject(&:published?).each do |asset|
+        asset.publish
+        log_and_wait :blue
+      end
+    end
+
+    # ---------------------------------------- | Unpublishing
+
+    def unpublish_content
+      env.entries.all(limit: 1000).to_a.reject { |e| e.fields[:title].blank? }.each do |entry|
+        entry.unpublish
+        log_and_wait
+      end
+      env.assets.all(limit: 1000).to_a.each do |asset|
+        asset.unpublish
+        log_and_wait :blue
+      end
+    end
+
+    # ---------------------------------------- | Deleting
 
     def delete_drafts
       %i[entries assets].each do |type|
         env.send(type).all(limit: 1000).reject(&:published?).each do |entry|
           entry.destroy
-          Logger.write('.', :green)
-          sleep 0.15
+          log_and_wait(type == :assets ? :blue : :green)
         end
-      end
-    end
-
-    def unpublish_content
-      collection = env.entries.all(limit: 1000).to_a.reject { |e| e.fields[:title].blank? } +
-                   env.assets.all(limit: 1000).to_a
-      collection.select(&:published?).each do |obj|
-        obj.unpublish
-        Logger.write('.', :green)
-        sleep 0.15
       end
     end
 
@@ -55,6 +75,15 @@ class Importer
 
     def env
       @env ||= client.environments(ENV['CONTENTFUL_SPACE_ID']).find(ENV['CONTENTFUL_ENV'])
+    end
+
+    def content_types
+      @content_types ||= env.content_types
+    end
+
+    def log_and_wait(color = :green)
+      Logger.write('.', color)
+      sleep 0.11
     end
   end
 
--- lib/tasks/migrate.rake
+++ lib/tasks/migrate.rake
@@ -1,15 +1,37 @@
-task :migrate do
-  Migrator.migrate(
-    '/series' => 'Series',
-    '/videos' => 'Video',
-    '/music' => 'Song'
-  )
+# ---------------------------------------- | Migrating
+
+task :migrate_series do
+  Migrator.migrate('/series' => 'Series')
 end
 
-task :delete do
-  Importer.delete_drafts
+task :migrate_music do
+  Migrator.migrate('/music' => 'Song')
 end
 
-task :unpublish do
+task :migrate_videos do
+  Migrator.migrate('/videos' => 'Video')
+end
+
+# ---------------------------------------- | Processing
+
+task :process_assets do
+  Importer.process_assets
+end
+
+# ---------------------------------------- | Publishing
+
+task :publish_assets do
+  Importer.publish_assets
+end
+
+# ---------------------------------------- | Unpublishing
+
+task :unpublish_content do
   Importer.unpublish_content
+end
+
+# ---------------------------------------- | Deleting
+
+task :delete_drafts do
+  Importer.delete_drafts
 end
